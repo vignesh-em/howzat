e113834929ef3c4e52f53564759fc2c7512ea053
Vignesh <emvignesh16@gmail.com>
2020-03-23 12:17:45 +0530

    Design Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./footer.css"
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className='footer-footer'>
+                <p className='p-copyright-text'>Copyright &copy; 2020</p>
+            </footer>
+        );
+    }
+}
+
+export default Footer
