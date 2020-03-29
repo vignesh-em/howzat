74835d0ecb3a12986d33fa33eb10235299dfbe17
Vignesh <emvignesh16@gmail.com>
2020-03-29 11:30:06 +0530

    Load current player details from state

--- src/components/CurrentPlayer/CurrentPlayer.js
+++ src/components/CurrentPlayer/CurrentPlayer.js
@@ -2,44 +2,92 @@ import React, {Component} from "react";
 import "./currentplayer.css";
 
 class CurrentPlayer extends Component {
+
+    state = {
+        player: {
+            firstName: "",
+            lastName: "",
+            matches: 0,
+            runs: 0,
+            strikeRate: 0.00,
+            highest: {
+                runs: 0,
+                notOut: false
+            },
+            wickets: 0,
+            economy: 0.00,
+            bestSpell: {
+                wickets: 0,
+                runs: 0
+            }
+        },
+        currentPrice: 0
+    };
+
+    componentDidMount() {
+        const newState = {
+            player: {
+                firstName: "Andre",
+                lastName: "Russel",
+                matches: 64,
+                runs: 1400,
+                strikeRate: 186.41,
+                highest: {
+                    runs: 88,
+                    notOut: true
+                },
+                wickets: 88,
+                economy: 8.88,
+                bestSpell: {
+                    wickets: 4,
+                    runs: 20
+                }
+            },
+            currentPrice: 200
+        };
+        this.setState(newState)
+    }
+
     render() {
+        const player = this.state.player;
+        const {highest, bestSpell} = player
         return (
             <>
                 <div className="price-tag">
-                    &#8377; 200L
+                    &#8377;{` ${this.state.currentPrice}L`}
                 </div>
                 <div className="current-player-name-container">
-                    <p className="current-player-first-name">Andre</p>
-                    <p className="current-player-last-name">Russel</p>
+                    <p className="current-player-first-name">{player.firstName}</p>
+                    <p className="current-player-last-name">{player.lastName}</p>
                 </div>
                 <div className="stats-container">
                     <div className="stat-item">
                         <p className="stat-label">Matches</p>
-                        <p className="stat-value">64</p>
+                        <p className="stat-value">{player.matches}</p>
                     </div>
                     <div className="stat-item">
                         <p className="stat-label">Runs</p>
-                        <p className="stat-value">1400</p>
+                        <p className="stat-value">{player.runs}</p>
                     </div>
                     <div className="stat-item">
                         <p className="stat-label">Strike Rate</p>
-                        <p className="stat-value">186.41</p>
+                        <p className="stat-value">{player.strikeRate}</p>
                     </div>
                     <div className="stat-item">
                         <p className="stat-label">Highest</p>
-                        <p className="stat-value">88*</p>
+                        <p className="stat-value">{highest.notOut ? `${highest.runs}*` : highest.runs}</p>
                     </div>
                     <div className="stat-item">
                         <p className="stat-label">Wickets</p>
-                        <p className="stat-value">55</p>
+                        <p className="stat-value">{player.wickets}</p>
                     </div>
                     <div className="stat-item">
                         <p className="stat-label">Economy</p>
-                        <p className="stat-value">8.88</p>
+                        <p className="stat-value">{player.economy}</p>
                     </div>
                     <div className="stat-item">
                         <p className="stat-label">Best Spell</p>
-                        <p className="stat-value">4/20</p>
+                        <p className="stat-value">{`${bestSpell.wickets}/${bestSpell.runs}`}</p>
                     </div>
                 </div>
             </>
