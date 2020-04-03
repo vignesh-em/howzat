607d004ae8cbe7762c92417dadb47cdc9039aa2b
Vignesh <emvignesh16@gmail.com>
2020-04-03 11:00:45 +0530

    Design sign-in component

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,14 +1,30 @@
 import React, {Component} from "react";
+import "./signinform.css";
 
 class SignInForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        console.log(event);
+    }
+
     render() {
         return (
             <form>
-                <div className="sign-up-container">
-                    <h4 style={{'text-align': 'center'}}>Signin Container</h4>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
+                <div className="sign-in-container">
+                    <div className="sign-in-input-field-container">
+                        <label htmlFor="username" className="sign-in-label">User Name</label>
+                        <input name="username" type="text" className="sign-in-input-text-field"/>
+                    </div>
+                    <div className="sign-in-input-field-container">
+                        <label htmlFor="password" className="sign-in-label">Password</label>
+                        <input name="password" type="password" className="sign-in-input-text-field"/>
+                    </div>
+                    <span className="sign-in-button" onClick={this.handleClick}>Sign In</span>
                 </div>
             </form>
         );
