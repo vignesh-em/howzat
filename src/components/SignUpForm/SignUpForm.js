e566f60fcc54c58b0ec3a5c5e5ade227bf88a47b
Vignesh <emvignesh16@gmail.com>
2020-04-14 11:05:17 +0530

    Add state to SignUpForm

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -2,14 +2,28 @@ import React, {Component} from "react";
 import "./signupform.css"
 
 class SignUpForm extends Component {
-
     constructor(props) {
         super(props);
+        this.state = {
+            userName: "",
+            teamName: "",
+            userKey: "",
+            password: "",
+            confirmPassword: "",
+        };
         this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleClick(event) {
-        console.log(event);
+        // TODO: Handle click
+    }
+
+    handleChange(event) {
+        const {target} = event;
+        this.setState({
+            [target.name]: target.value
+        });
     }
 
     render() {
@@ -17,24 +31,44 @@ class SignUpForm extends Component {
             <form>
                 <div className="sign-up-container">
                     <div className="sign-up-input-field-container">
-                        <label htmlFor="username" className="sign-up-label">User Name</label>
-                        <input name="username" type="text" className="sign-up-input-text-field"/>
+                        <label htmlFor="userName" className="sign-up-label">User Name</label>
+                        <input
+                            name="userName"
+                            type="text"
+                            onChange={this.handleChange}
+                            className="sign-up-input-text-field"/>
                     </div>
                     <div className="sign-up-input-field-container">
-                        <label htmlFor="teamname" className="sign-up-label">Team Name</label>
-                        <input name="teamname" type="text" className="sign-up-input-text-field"/>
+                        <label htmlFor="teamName" className="sign-up-label">Team Name</label>
+                        <input
+                            name="teamName"
+                            onChange={this.handleChange}
+                            type="text"
+                            className="sign-up-input-text-field"/>
                     </div>
                     <div className="sign-up-input-field-container">
-                        <label htmlFor="userkey" className="sign-up-label">User Key</label>
-                        <input name="userkey" type="text" className="sign-up-input-text-field"/>
+                        <label htmlFor="userKey" className="sign-up-label">User Key</label>
+                        <input
+                            name="userKey"
+                            onChange={this.handleChange}
+                            type="text"
+                            className="sign-up-input-text-field"/>
                     </div>
                     <div className="sign-up-input-field-container">
                         <label htmlFor="password" className="sign-up-label">Password</label>
-                        <input name="password" type="password" className="sign-up-input-text-field"/>
+                        <input
+                            name="password"
+                            onChange={this.handleChange}
+                            type="password"
+                            className="sign-up-input-text-field"/>
                     </div>
                     <div className="sign-up-input-field-container">
-                        <label htmlFor="confirmpassword" className="sign-up-label">Confirm Password</label>
-                        <input name="confirmpassword" type="password" className="sign-up-input-text-field"/>
+                        <label htmlFor="confirmPassword" className="sign-up-label">Confirm Password</label>
+                        <input
+                            name="confirmPassword"
+                            onChange={this.handleChange}
+                            type="password"
+                            className="sign-up-input-text-field"/>
                     </div>
 
                     <span className="sign-up-button" onClick={this.handleClick}>Sign Up</span>
