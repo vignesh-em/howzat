8a10d5c3cdae26dc4a62530bfc2d558fbb36d2ae
Vignesh <emvignesh16@gmail.com>
2020-04-14 07:53:54 +0530

    Refactor Header to functional component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,35 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import "./header.css";
 import {Link} from "react-router-dom";
 
-class Header extends Component {
+const Header = () => {
+    const [loggedIn, setLoggedIn] = useState(true);
+    const [navItems] = useState(true);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: props.isLoggedIn === "true",
-            showNavItems: props.showNavItems === "true"
-        }
-    }
-
-    componentDidMount() {
-        console.log(this.state);
-    }
-
-    render() {
-        const state = this.state;
-        return (
-            <header id="header-main">
-                <div id="title-container">
-                    <h1 id="app-title">howzat?</h1>
-                    <p id="app-subtitle">auction game</p>
-                </div>
-                {
-                    state.showNavItems &&
-                    <nav id="nav-bar">
-                        <ul className="header-list">
-                            {
-                                state.isLoggedIn ?
-                                    <li className="header-nav-item"><a href="#">logout</a></li> :
-                                    <li className="header-nav-item"><Link to="/login">login</Link></li>
-                            }
-                        </ul>
-                    </nav>
-                }
-            </header>
-        );
-    }
+    return (
+        <header id="header-main">
+            <div id="title-container">
+                <h1 id="app-title">howzat?</h1>
+                <p id="app-subtitle">auction game</p>
+            </div>
+            {
+                navItems &&
+                <nav id="nav-bar">
+                    <ul className="header-list">
+                        {
+                            loggedIn ?
+                                <li className="header-nav-item" onClick={() => setLoggedIn(false)}>
+                                    <Link to="/">logout</Link>
+                                </li> :
+                                <li className="header-nav-item">
+                                    <Link to="/login">login</Link>
+                                </li>
+                        }
+                    </ul>
+                </nav>
+            }
+        </header>
+    );
 }
 
 export default Header;
