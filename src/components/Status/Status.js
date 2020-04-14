24bfed649774f49cfd1df7f37caf9853bfc969aa
Vignesh <emvignesh16@gmail.com>
2020-04-14 08:29:01 +0530

    Refactor Status component

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -1,13 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
 import "./status.css";
 
-class Status extends Component {
-    render() {
-        return (
-            <p className="live-status">Sold to <span className="status-bought-team-name">Kolkata Knight Riders</span>!
-            </p>
-        );
-    }
-}
+const Status = () => (
+    <p className="live-status">
+        Sold to <span className="status-bought-team-name">Kolkata Knight Riders</span>!
+    </p>
+);
 
 export default Status;
