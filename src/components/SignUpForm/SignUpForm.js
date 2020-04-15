294d4eef0f7892e702cddee232b81504a191741f
Vignesh <emvignesh16@gmail.com>
2020-04-16 05:02:17 +0530

    Refactor SignUpForm using hooks

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,81 +1,67 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 import "./signupform.css"
 
-class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userName: "",
-            teamName: "",
-            userKey: "",
-            password: "",
-            confirmPassword: "",
-        };
-        this.handleClick = this.handleClick.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+const SignUpForm = () => {
+    const [userName, setUserName] = useState();
+    const [teamName, setTeamName] = useState();
+    const [userKey, setUserKey] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
+    const [isSignUpClicked, setIsSignUpClicked] = useState(false);
 
-    handleClick(event) {
-        // TODO: Handle click
-    }
+    useEffect(() => {
+        if (isSignUpClicked) {
 
-    handleChange(event) {
-        const {target} = event;
-        this.setState({
-            [target.name]: target.value
-        });
-    }
+        }
+    }, [isSignUpClicked])
 
-    render() {
-        return (
-            <form>
-                <div className="sign-up-container">
-                    <div className="sign-up-input-field-container">
-                        <label htmlFor="userName" className="sign-up-label">User Name</label>
-                        <input
-                            name="userName"
-                            type="text"
-                            onChange={this.handleChange}
-                            className="sign-up-input-text-field"/>
-                    </div>
-                    <div className="sign-up-input-field-container">
-                        <label htmlFor="teamName" className="sign-up-label">Team Name</label>
-                        <input
-                            name="teamName"
-                            onChange={this.handleChange}
-                            type="text"
-                            className="sign-up-input-text-field"/>
-                    </div>
-                    <div className="sign-up-input-field-container">
-                        <label htmlFor="userKey" className="sign-up-label">User Key</label>
-                        <input
-                            name="userKey"
-                            onChange={this.handleChange}
-                            type="text"
-                            className="sign-up-input-text-field"/>
-                    </div>
-                    <div className="sign-up-input-field-container">
-                        <label htmlFor="password" className="sign-up-label">Password</label>
-                        <input
-                            name="password"
-                            onChange={this.handleChange}
-                            type="password"
-                            className="sign-up-input-text-field"/>
-                    </div>
-                    <div className="sign-up-input-field-container">
-                        <label htmlFor="confirmPassword" className="sign-up-label">Confirm Password</label>
-                        <input
-                            name="confirmPassword"
-                            onChange={this.handleChange}
-                            type="password"
-                            className="sign-up-input-text-field"/>
-                    </div>
-
-                    <span className="sign-up-button" onClick={this.handleClick}>Sign Up</span>
+    return (
+        <form>
+            <div className="sign-up-container">
+                <div className="sign-up-input-field-container">
+                    <label htmlFor="userName" className="sign-up-label">User Name</label>
+                    <input
+                        name="userName"
+                        type="text"
+                        onChange={({target}) => setUserName(target.value)}
+                        className="sign-up-input-text-field"/>
                 </div>
-            </form>
-        );
-    }
+                <div className="sign-up-input-field-container">
+                    <label htmlFor="teamName" className="sign-up-label">Team Name</label>
+                    <input
+                        name="teamName"
+                        onChange={({target}) => setTeamName(target.value)}
+                        type="text"
+                        className="sign-up-input-text-field"/>
+                </div>
+                <div className="sign-up-input-field-container">
+                    <label htmlFor="userKey" className="sign-up-label">User Key</label>
+                    <input
+                        name="userKey"
+                        onChange={({target}) => setUserKey(target.value)}
+                        type="text"
+                        className="sign-up-input-text-field"/>
+                </div>
+                <div className="sign-up-input-field-container">
+                    <label htmlFor="password" className="sign-up-label">Password</label>
+                    <input
+                        name="password"
+                        onChange={({target}) => setPassword(target.value)}
+                        type="password"
+                        className="sign-up-input-text-field"/>
+                </div>
+                <div className="sign-up-input-field-container">
+                    <label htmlFor="confirmPassword" className="sign-up-label">Confirm Password</label>
+                    <input
+                        name="confirmPassword"
+                        onChange={({target}) => setConfirmPassword(target.value)}
+                        type="password"
+                        className="sign-up-input-text-field"/>
+                </div>
+                <span className="sign-up-button" onClick={() => setIsSignUpClicked(true)}>Sign Up</span>
+            </div>
+        </form>
+    );
 }
 
 export default SignUpForm;
